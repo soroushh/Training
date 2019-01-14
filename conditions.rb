f73587ab201a8ab117701c1d615ea58fb6914fa4
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-14 11:34:49 +0000

    A new method is written in the file.

--- conditions.rb
+++ conditions.rb
@@ -9,3 +9,19 @@ class Integer
 end
 
 puts 5.status()
+
+def accumulating(a)
+	i=1
+	while true
+		if a%i == 0
+			puts i
+		end
+		if i==a
+			break
+		end
+		i+=1
+	end
+end
+
+accumulating(144)
+
