bc052bb535567629d9aa4657ed941e37f9cea8a0
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-16 16:44:11 +0000

    A singleton method.

--- WGR.rb
+++ WGR.rb
@@ -1,30 +1,10 @@
-# Showing instance variables
+obj = Object.new()
 
-class Person
-  @@all = []
-  def initialize(name , family)
-    @name = name
-    @family = family
-    @@all << self
-  end
-  def to_s
-    return "The person is #{@name} #{@family}."
-  end
-  def self.all
-    @@all
-  end
+def obj.help()
+  puts "This is a singleton method."
 end
 
-def args(a,b=2,*c,d)
-  puts "a is #{a}, b is #{b} and c is #{c} and d is #{d}"
-end
-
-def method(a)
-  puts a
-end
-
-puts a 
-
+obj.help()
 
 
 
