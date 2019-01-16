a359cf7973f44122d06f508ae0120ab9d3c7d026
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-16 16:54:53 +0000

    How to make definite objects in a class using array.

--- WGR.rb
+++ WGR.rb
@@ -1,12 +1,35 @@
-obj = Object.new()
-
-def obj.help()
-  puts "This is a singleton method."
+class Person
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+  def address=(a)
+    @adress = a
+  end
+  def address
+    @adress
+  end
+  def self.all
+    @all ||= []
+  end
+  def to_s
+    "The name of the object is #{@name}"
+  end
 end
+puts "Please enter the name of persons?"
 
-obj.help()
-
-
+while true
+  name = gets.chomp()
+  if name == "stop"
+    break
+  else
+    Person.new(name)
+  end
+end
+Person.all.each{
+  |x|
+  puts x
+}
 
 
 
