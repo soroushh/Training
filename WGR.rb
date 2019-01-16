79357b95aea7b79d69098dde39bf1be5640874ff
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-16 08:27:58 +0000

    class variable added.

--- WGR.rb
+++ WGR.rb
@@ -1,16 +1,26 @@
 # Showing instance variables
 
 class Person
+  @@all = []
   def initialize(name , family)
     @name = name
     @family = family
+    @@all << self
   end
   def to_s
     return "The person is #{@name} #{@family}."
+  end
+  def self.all
+    @@all
   end
 end
 
 person_1 = Person.new("soroush", "khosravi")
 
-puts person_1
+person_2 = Person.new("farnaz","Ostovari")
+
+Person.all.each{
+  |x|
+  puts x 
+}
 
