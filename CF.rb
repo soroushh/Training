7b97d70334ebc09f834aa8e3f355116d4996bd8a
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-18 09:02:03 +0000

    raising exceptions.

--- CF.rb
+++ CF.rb
@@ -1,33 +1,8 @@
-class Array
-  def my_each
-    size = self.length
-    i = 0
-    while i < size
-      yield(self[i])
-      i+=1
-    end
-  end
-  def my_map
-    c =0
-    ar = []
-    size = self.length
-    while c<size
-      ar << yield(self[c])
-      c +=1
-    end
-    return ar
-  end
+puts "input?"
+
+a = gets.chomp.to_i
+
+if a >10
+  raise RuntimeError 
 end
-
-ar = [1,2,3,4]
-
-b = ar.my_map{
-  |x|
-  x**2
-
-}
-p b 
-
-
-
 
