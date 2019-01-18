42977ce517a7777d44a334924edee55801123bae
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-18 15:04:49 +0000

    select and reject methods for a hash.

--- hash.rb
+++ hash.rb
@@ -1,24 +1,18 @@
-puts "Please enter the sentence."
-
-sentence = gets.chomp()
-
-ar = sentence.gsub("."," ").gsub("?"," ").split(" ")
-
-un = ar.uniq
-
-hash = Hash.new(0)
-
-for i in un
-  for j in ar
-    if j == i
-      hash[j]+=1
-    end
-  end
-end
-
-b = hash.sort_by{
+hash = {
+  "soroush" => "farnaz",
+  "rostam" => "katayoun",
+  "sohrab" => "farah"
+}
+a = hash.reject{
   |x,y|
-  y
+  y > "g"
 }
 
-p b.to_h 
+p a 
+
+b = hash.select{
+  |x,y|
+  y > "g"
+}
+
+p b 
