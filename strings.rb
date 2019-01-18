7880ba37beeb680d4f198e986edc72e18d8b8164
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-18 09:30:29 +0000

    how to make comparison factors

--- strings.rb
+++ strings.rb
@@ -1,11 +1,16 @@
-puts "Please enter the number"
-
-begin
-  a = gets.chomp()
-  b = Integer(a)
-rescue
-  puts "The input should be an integer"
-  exit
+class Person
+  include Comparable
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+  def <=> sec
+    self.name <=> sec.name
+  end
 end
 
-puts "The integer is #{b}"
+person_1 = Person.new("b")
+
+person_2 = Person.new("c")
+
+puts person_1 > person_2
