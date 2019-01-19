b223bfee84d48ff9918a476930e69f50671492d5
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-19 12:15:09 +0000

    using subset and superset methods for sets.

--- sets.rb
+++ sets.rb
@@ -1,11 +1,14 @@
 require 'set'
 
-set_1 = Set.new(["a","b"])
 
-ar = ["c","d"]
+set_1 = Set.new(["a","b","c","d","e"])
 
-hash = { 1=>2}
+set_2 = Set.new(["b","c","e"])
 
-p set_1.merge(ar)
+p set_1.superset?(set_2)
 
-p set_1.merge(hash)
+p set_2.superset?(set_1)
+
+p set_1.subset?(set_2)
+
+p set_2.subset?(set_1)
