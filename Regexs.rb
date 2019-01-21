4cc2f9de79573298d98d976da02fbbd130976ed3
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-21 13:59:53 +0000

    look ahead and look behind assertions.

--- Regexs.rb
+++ Regexs.rb
@@ -1,18 +1,13 @@
-str = "this is a string with several lines.\n This is the second line.\n This is the third line."
+str = "soroush khosravi telephone number is 0098 .353 8212579."
 
-re =/\w+\.$/
+re1 = /\w+\s(?=\w{6})/
 
-re2 =/^\s\w+/
+re2 = /(?<=\.)\d+/
 
-re3=/\b\w\b/
+p re1.match(str)[0]
 
-re4 = /\w+\.\Z/
 
-p str.match(re)[0]
+p re2.match(str)[0]
 
-p str.match(re2)[0]
 
-p str.match(re3)[0]
-
-p str.match(re4)[0]
 
