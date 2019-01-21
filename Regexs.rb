758b29a937dcb44d65b032297fa3a6918666c6d2
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-21 10:23:06 +0000

    Matching, substring and matchdata.

--- Regexs.rb
+++ Regexs.rb
@@ -1,5 +1,18 @@
-a = "This a phone number with country code:0098 351 8212579."
+name = "Mr, Soroush , Khosravi"
 
-p a.match(/\d{4}\s\d{3}\s(\d{7})/)[1]
+a = name.match(/(Mrs?)\s*,*\s*([a-zA-Z]+)\s*,*\s*([a-zA-Z]+)/)
 
-p a.match(/\d{4}\s(\w{3})\s\d{7}/)[1]
+p a 
+
+p a[1]
+
+p a[2]
+
+p a[3]
+
+p $1
+
+p $2
+
+
+p $3
