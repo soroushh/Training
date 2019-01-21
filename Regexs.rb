531426c5eaa3b0f8e621837053cfc4583cf75564
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-21 10:12:46 +0000

    sequences for common character classes.

--- Regexs.rb
+++ Regexs.rb
@@ -1,3 +1,5 @@
-a = "This is String with a dot(.) in it.\n line."
+a = "This a phone number with country code:0098 351 8212579."
 
-p a.match(/([A-Z][a-z]{5})/)[1]
+p a.match(/\d{4}\s\d{3}\s(\d{7})/)[1]
+
+p a.match(/\d{4}\s(\w{3})\s\d{7}/)[1]
