8fe63dc12210a0359134482679959bb1b6f7a124
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-21 09:11:13 +0000

    Using enum_for for inject method.

--- Theenum.rb
+++ Theenum.rb
@@ -1,32 +1,10 @@
-ar = ["a", "b", "c", "d", "e"]
+names = %w{Soroush Farnaz Rostam Katayoun}
 
-enum = ar.enum_for(:select)
+e = names.enum_for(:inject, "Names...")
 
-sel = enum.each{
-  |x|
-  x > "b"
-}
-
-p sel
-
-enum_2 = ar.enum_for(:collect)
-
-col = enum_2.each{
-  |x|
-  x*2
-}
-
-p col 
-
-hash = {"a"=>"b",
-  "c"=> "d",
-}
-
-enum_3 = hash.enum_for(:select)
-
-ha = enum_3.each{
+a = e.each{
   |x,y|
-  x > "a"
+  x + y + "..."
 }
 
-puts ha
+puts a
