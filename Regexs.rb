f41d1a7cacdad883ae54a5a1436351ed1da6d0f1
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-21 11:25:46 +0000

    Naming captures.

--- Regexs.rb
+++ Regexs.rb
@@ -1,16 +1,16 @@
 name = "Mr, Soroush , Khosravi is the first person. Mrs , farnaz ,   Ostovari is the second person."
 
-ar = name.match(/(Mrs?)\s*,*\s*([a-zA-Z]+)\s*,*\s*([a-zA-Z]+)/)
+ar = name.match(/Mrs?\s*,*\s*(?<name>[a-zA-Z]+)\s*,*\s*[a-zA-Z]+/)
 
-puts ar.captures[0]
+puts ar[:name]
 
 puts ar[0]
 
 puts ar[1]
 
-puts ar[2]
+puts ar.captures[0]
 
-puts ar[3]
 
-puts ar.captures[1]
+
+
 
