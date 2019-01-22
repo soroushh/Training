60fe9cab2a133c5916083431ea538e2c66192d26
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-22 08:47:51 +0000

    lookahead, lookbehind and not-counter parentheses.

--- Regexs.rb
+++ Regexs.rb
@@ -1,13 +1,19 @@
 str = "soroush khosravi telephone number is 0098 .353 8212579."
 
-re1 = /\w+\s(?=\w{6})/
+re1 =/(?<=0098\s)(\.\d{3})/
 
-re2 = /(?<=\.)\d+/
+re2 = /(\.\d{3})(?=\s\d+)/
 
-p re1.match(str)[0]
+re3 = /(\w+)\s(?:\w+)\s(\w+)/
 
+p str.match(re1)[0]
 
-p re2.match(str)[0]
+p str.match(re2)[0]
 
+p str.match(re3)[0]
+
+p str.match(re3)[1]
+
+p str.match(re3)[2]
 
 
