b76654dcaf935524378e9d4f3c501f3ff833a43a
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-26 12:47:52 +0000

    string scaling

--- inp.rb
+++ inp.rb
@@ -1,15 +1,34 @@
-def generate_shape(5)
-  i=
-  str=""
-  while i <= n
-   if i == n
-     str += "+"*n
-   else
-   str += "+"*n+"\n"
-   end
-   i+=1
-  end 
-  return str
+def scale(strng, k, n)
+  if strng == ""
+    return ""
+  else
+    ar_1 = strng.split("\n")
+    ar_2 = []
+    ar_1.each{
+      |x|
+      ar_2.push(x.split(""))
+    }
+    ar_3 = []
+    ar_2.each{
+      |x|
+      str =""
+      x.each{
+        |y|
+        str<<(y*k)
+      }
+      ar_3.push(str)
+    }
+    ar_4 = []
+    ar_3.each{
+      |x|
+      n.times{
+        ar_4.push(x)
+      }
+    }
+    return ar_4.join("\n")
+  end    
 end
 
-puts generate_shape(5)
+a = "abcd\nefgh\nijkl\nmnop"
+
+p scale(a,2,3)
