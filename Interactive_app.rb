274fc0cea5057cf6c34ef7190a2c73c58272d482
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-29 15:13:08 +0000

    Menu method added to the file.

--- Interactive_app.rb
+++ Interactive_app.rb
@@ -1,7 +1,7 @@
-def a()
-  puts b
+def print_menu
+  puts " Please enter the action you want to do."
+  puts "1. Entering some people name and family."
+  puts "2. Showing the people's name"
+  puts "Loading to a file."
+  puts "Importing from a file."
 end
-def b
-  "hey"
-end
-a
