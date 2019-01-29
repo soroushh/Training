6a298e4daa94a2155fbfc0589ce0d3de33e8d3db
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-29 15:22:10 +0000

    The input method written.

--- Interactive_app.rb
+++ Interactive_app.rb
@@ -2,6 +2,34 @@ def print_menu
   puts " Please enter the action you want to do."
   puts "1. Entering some people name and family."
   puts "2. Showing the people's name"
-  puts "Loading to a file."
-  puts "Importing from a file."
+  puts "3. Loading to a file."
+  puts "4. Importing from a file."
+  puts "5. Exit"
 end
+def input(ar=[])
+  while true
+    puts "Please enter the name. If finished, press the return."
+    name = gets.chomp()
+    if name ==""
+      break
+    end
+    puts "Please enter the family."
+    family = gets.chomp()
+    ar << {name: name, family: family}
+    puts "We have #{ar.count} persons."
+  end
+end
+def interactive_app()
+  ar = []
+  while true
+    print_menu
+    option = gets.chomp
+    if option == "1"
+      input(ar)
+    elsif option =="5"
+      exit
+    end
+  end
+end
+
+interactive_app
