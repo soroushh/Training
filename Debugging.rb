624ecc57817c3da8db2c97128cab3846e12551d0
Soroush Khosravi <sorush.kh68@gmail.com>
2019-02-06 09:48:23 +0000

    Excercise 3

--- Debugging.rb
+++ Debugging.rb
@@ -1,10 +1,13 @@
 def factorial(n)
   product = 1
-  while n > 0
+  p "at the start product is #{product}"
+  while n > 1
+    p "we multiply #{product} by #{n}"
     product *= n
+    p "we get #{product}"
     n -= 1
   end
   product
 end
 
-p factorial(5)
+factorial(5)
