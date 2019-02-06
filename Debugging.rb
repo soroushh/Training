ab046481c596fad3cdbf029422d9bc1409311fb5
Soroush Khosravi <sorush.kh68@gmail.com>
2019-02-06 09:44:03 +0000

    exercise 3

--- Debugging.rb
+++ Debugging.rb
@@ -1,28 +1,10 @@
-def encode(plaintext, key)
-  # cipher is an array of key characters plus array of "a" to "z" minus key characters.
-  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
-
-  ciphertext_chars = plaintext.chars.map do |char|
-    (65 + cipher.find_index(char)).chr
+def factorial(n)
+  product = 1
+  while n > 0
+    product *= n
+    n -= 1
   end
-  ciphertext_chars.join
+  product
 end
 
-def decode(ciphertext, key)
-  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
-  plaintext_chars = ciphertext.chars.map do |char|
-    cipher[char.ord - 65]
-  end
-    plaintext_chars.join()
-  # plaintext_chars.join
-
-end
-
-# Intended output:
-#
-# > encode("theswiftfoxjumpedoverthelazydog", "secretkey")
-# => "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
-#
-# > decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
-# => "theswiftfoxjumpedoverthelazydog"
-p encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+p factorial(5)
